12d7163692072b2b7729e14d0464742e844b2e2e
JannikRecklies <jannik.recklies@gmx.de>
2024-04-17 18:56:19 +0200

    Player now also dies when reaching end of river.

--- Assets/Scripts/KillPlayerOnTouch.cs
+++ Assets/Scripts/KillPlayerOnTouch.cs
@@ -12,4 +12,12 @@ public class KillPlayerOnTouch : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<Player>() != null && transform.CompareTag("EdgeOfMap"))
+        {
+            FollowPlayer cam = GameObject.Find("Main Camera").GetComponent<FollowPlayer>();
+            cam.StopFollowingPlayer();
+        }
+    }
 }

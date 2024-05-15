f0c8c43c22115e6d925083806313add85abbd4db
Clarissa Röhrdanz <clarissalorena@MacBook-Pro-von-Clarissa.local>
2024-05-15 12:38:39 +0200

    script for gameover screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    public Text points;
+    // Start is called before the first frame update
+    public void Setup(int score) {
+        gameObject.SetActive(true);
+        pointsText.text = "Score: " + score.ToString();
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene("MainScene");
+    }
+}

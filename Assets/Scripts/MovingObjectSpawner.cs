49b130ba2fe9e75af16bcb17bd7bae0472bd8fb9
JannikRecklies <jannik.recklies@gmx.de>
2024-05-19 21:37:18 +0200

    Added spawn terrains with trees. Changed spawn of objects to always have grass inbetween. Added objects to also spawn in the middle at the beginning. Some bugs with logs exist now.

--- Assets/Scripts/MovingObjectSpawner.cs
+++ Assets/Scripts/MovingObjectSpawner.cs
@@ -1,32 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MovingObjectSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject movingObject;
     [SerializeField] private Transform spawnPos;
-    [SerializeField] private float minSperarationTime;
-    [SerializeField] private float maxSperarationTime;
+    [SerializeField] private int minSperarationTime;
+    [SerializeField] private int maxSperarationTime;
     [SerializeField] private bool isRightSide;
 
     void Start()
     {
-        StartCoroutine(SpawnObject());
+        Vector3 temp = spawnPos.position;
+        int randomInitialSpawnPos = Mathf.RoundToInt(Random.Range(spawnPos.position.z - 20, spawnPos.position.z));
+        temp.z = randomInitialSpawnPos;
+        SpawnObject(temp);
+        StartCoroutine(SpawnObjectRoutine());
     }
 
-    // Update is called once per frame
-    private IEnumerator SpawnObject()
-    {
+    private IEnumerator SpawnObjectRoutine() {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(minSperarationTime, maxSperarationTime));
-            GameObject go = Instantiate(movingObject, spawnPos.position, Quaternion.identity);
-            if (!isRightSide)
-            {
-                go.transform.Rotate(new Vector3(0, 180, 0));
-            }
+            int waitTime = Random.Range(minSperarationTime, maxSperarationTime);
+            yield return new WaitForSeconds(Mathf.RoundToInt(waitTime));
+            SpawnObject(spawnPos.position);
+        }
+    }
+
+
+    // Update is called once per frame
+    private void SpawnObject(Vector3 spawnPosition)
+    {
+        GameObject go = Instantiate(movingObject, spawnPosition, Quaternion.identity);
+        if (!isRightSide)
+        {
+            go.transform.Rotate(new Vector3(0, 180, 0));
         }
     }
 }

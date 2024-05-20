b895e8359c9d5a165e511d45020522d9767eee91
JannikRecklies <jannik.recklies@gmx.de>
2024-05-20 22:41:15 +0200

    Added pause option via keyboard for the game.

--- Assets/Scripts/GamerController.cs
+++ Assets/Scripts/GamerController.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class GamerController : MonoBehaviour
 {
     //[SerializeField] private GameStartScreen gameStartScreen;
-    //[SerializeField] private GamePauseScreen gamePauseScreen;
+    [SerializeField] private PauseMenu pauseMenu;
     [SerializeField] private GameOverScreen gameOverScreen;
+
+    private bool gameRunning = false;
 
     private void Start()
     {
+        gameRunning = true;
         GameEvents.OnPlayerDied += ShowGameOverScreen;
     }
 
+    private void Update() {
+        if (gameRunning && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))) {
+            pauseMenu.PauseGame();
+        }
+    }
+
     private void ShowGameOverScreen(int score) {
+        gameRunning = false;
         gameOverScreen.SetUp(score);
     }
 

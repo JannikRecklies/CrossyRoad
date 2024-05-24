adf0cc303ddd7697734209a0e533f32a7e32615c
Clarissa Röhrdanz <clarissalorena@student-220-178.eduroam.uu.se>
2024-05-24 16:26:31 +0200

    LocalizedTextTMP script and added components for texts

---
+++ Assets/SimpleLocalization/Scripts/LocalizeTextTMP.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+namespace Assets.SimpleLocalization.Scripts
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizedTextTMP : MonoBehaviour
+    {
+        public string LocalizationKey;
+
+        public void Start()
+        {
+            Localize();
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            GetComponent<TextMeshProUGUI>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+    }
+}
